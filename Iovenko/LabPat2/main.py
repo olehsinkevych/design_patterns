8250a8f27ae30ba9080c389c078536f634faf768
Climp228 <55872678+Climp228@users.noreply.github.com>
2020-10-09 09:13:59 +0300

    Add files via upload

---
+++ Iovenko/LabPat2/main.py
@@ -0,0 +1,81 @@
+class Staff:
+    def __init__(self, name: str, adress: str, school: str, pay: float):
+        self.name = name
+        self.adress = adress
+        self.school = school
+        self.pay = pay
+
+    def get_School(self):
+        return self.school
+
+    def set_School(self, school: str):
+        self.school = school
+
+    def get_Pay(self):
+        return self.pay
+
+    def set_Pay(self, pay: float):
+        self.pay = pay
+
+    def __str__(self):
+        return "Staff -   [Person [name = " + self.name + ", adress = " + self.adress + "], school = " \
+               + self.school + ", pay = " + str(self.pay) + "$]"
+
+
+class Student:
+    def __init__(self, name: str, adress: str, program: str, year: int, fee: float):
+        self.name = name
+        self.adress = adress
+        self.program = program
+        self.year = year
+        self.fee = fee
+
+    def get_Program(self):
+        return self.program
+
+    def set_Program(self, program: str):
+        self.program = program
+
+    def get_Year(self):
+        return self.year
+
+    def set_Year(self, year: int):
+        self.year = year
+
+    def get_Fee(self):
+        return self.fee
+
+    def set_Fee(self, fee: float):
+        self.fee = fee
+
+    def __str__(self):
+        return "Student - [Person [name = " + self.name + ", adress = " + self.adress + \
+               "], program = " + self.program + ", year = " + str(self.year) + ", fee = " + str(self.fee) + "$]"
+
+
+class Person:
+    def __init__(self, name: str, adress: str):
+        self.name = name
+        self.adress = adress
+
+    def get_Name(self):
+        return self.name
+
+    def get_Adress(self):
+        return self.adress
+
+    def set_Adress(self, adress: str):
+        self.adress = adress
+
+    def __str__(self):
+        return "Person -  [name = " + self.name + ", adress = " + self.adress + "]"
+
+
+mypers = Person("Arsen", "Kyiv")
+print(mypers)
+
+myStaff = Staff("Dima", "Dnipro", "Dnipro_School", 500)
+print(myStaff)
+
+mystudent = Student("Oleh", "Lviv", "Electronik", 2020, 200)
+print(mystudent)
