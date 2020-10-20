11605520d55ec27def288d1e7808cf01f67d42d2
strongspirit <58071029+strongspirit@users.noreply.github.com>
2020-10-20 13:49:03 +0300

    Delete main.py

--- Plotnikov/main.py
+++
@@ -1,9 +0,0 @@
-from person import Person, Student, Staff
-
-student = Person("Artur", "Lviv, Drahomanova 50")
-
-student = Student(student.name, student.address, "Belles", 4, 7)
-student.Student()
-
-student = Staff(student.name, student.address, "4", 17)
-student.Staff()
