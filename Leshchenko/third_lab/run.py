600698d150c541aea806e304ee2bc37ad6377d84
leshchenkoM <57502160+leshchenkoM@users.noreply.github.com>
2020-11-25 13:09:03 +0200

    Add files via upload

---
+++ Leshchenko/third_lab/run.py
@@ -0,0 +1,11 @@
+from main import ProductFactory, CappuccinoFactory, Cappuccino,HotMilkFactory, Customization, Preparation
+
+
+cust = Customization(1, 3, 2)
+factory = HotMilkFactory()
+cappuccino = factory.get_product(cust)
+
+
+
+
+
