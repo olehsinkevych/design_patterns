ab125a04e414dd1125b4618013cb0804f0a5d545
MAXST123 <46112442+MAXST123@users.noreply.github.com>
2020-12-08 15:17:30 +0200

    Add files via upload

---
+++ Max Stefankiv/lab_4/department.py
@@ -0,0 +1,11 @@
+from category import Category
+
+
+class Department:
+    def __init__(self, department_id: Category(), name: str, description: Category()):
+        self.department_id = department_id
+        self.name = name
+        self.description = description
+
+    def category_in_department(self):
+        pass
