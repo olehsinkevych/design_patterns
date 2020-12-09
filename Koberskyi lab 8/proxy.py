493908c312b1ea941b843b0181b27fb9dfb96634
Danylo-cyber <55576249+Danylo-cyber@users.noreply.github.com>
2020-12-09 13:49:59 +0200

    Add files via upload

---
+++ Koberskyi lab 8/proxy.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+import datetime
+
+
+class IBook(metaclass=ABCMeta):
+    @abstractmethod
+    def getName(self):
+        """Name of the book"""
+        pass
+
+    @abstractmethod
+    def getAuthor(self):
+        """Author of the book"""
+        pass
+
+    @abstractmethod
+    def isAvailable(self):
+        """Do we have the book"""
+        pass
+
+
+class HeavyBook(IBook):
+    def method(self):
+        self.getName()
+        self.getAuthor()
+        self.isAvailable()
+
+    def isAvailable(self):
+        print("Yes")
+
+    def getAuthor(self):
+        print("Tolstoi")
+
+    def getName(self):
+        print("War and Peace")
+
+
+class LightBook(IBook):
+    def __init__(self):
+        self.book = HeavyBook()
+
+    def isAvailable(self):
+        print("No")
+
+    def getAuthor(self):
+        print("Tolstoi")
+
+    def getName(self):
+        print("")
+
+    def method(self, weight="light"):
+        if weight == "light":
+            self.getAuthor()
+            self.isAvailable()
+        else:
+            self.book.method()
+
+
+print("\nStandart:")
+book = HeavyBook()
+book.method()
+print("\nProxied:")
+book = LightBook()
+book.method()
+print("\nRequested:")
+book.method("heavy")
