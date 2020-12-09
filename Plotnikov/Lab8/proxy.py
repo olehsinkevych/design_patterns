ee55ed444dca4f83854c43d0e281bbf3b9a9711e
strongspirit <58071029+strongspirit@users.noreply.github.com>
2020-12-09 13:54:58 +0200

    Add files via upload

---
+++ Plotnikov/Lab8/proxy.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+import datetime
+
+
+class IBook(metaclass=ABCMeta):
+    @abstractmethod
+    def getName(self):
+        """Name of the book"""
+        pass
+
+    @abstractmethod
+    def getAuthor(self):
+        """Author of the book"""
+        pass
+
+    @abstractmethod
+    def isAvailable(self):
+        """Do we have the book"""
+        pass
+
+
+class HeavyBook(IBook):
+    def method(self):
+        self.getName()
+        self.getAuthor()
+        self.isAvailable()
+
+    def isAvailable(self):
+        print("Yes")
+
+    def getAuthor(self):
+        print("MarkTwain")
+
+    def getName(self):
+        print("A Connecticut Yankee in King Arthur’s Court")
+
+
+class LightBook(IBook):
+    def __init__(self):
+        self.book = HeavyBook()
+
+    def isAvailable(self):
+        print("No")
+
+    def getAuthor(self):
+        print("MarkTwein")
+
+    def getName(self):
+        print("")
+
+    def method(self, weight="light"):
+        if weight == "light":
+            self.getAuthor()
+            self.isAvailable()
+        else:
+            self.book.method()
+
+
+print("\nStandart:")
+book = HeavyBook()
+book.method()
+print("\nProxied:")
+book = LightBook()
+book.method()
+print("\nRequested:")
+book.method("heavy")
