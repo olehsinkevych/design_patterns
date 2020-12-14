faa81db00d7abe4a75c9d7c8df165ddd3661fd4d
TopYozhik <44850575+TopYozhik@users.noreply.github.com>
2020-12-14 22:24:16 +0200

    Add files via upload

---
+++ Izhak/lab4/shipping_info.py
@@ -0,0 +1,9 @@
+class ShippingInfo:
+    def __init__(self, shipping_id: int, shipping_type: str, shipping_cost: int, shipping_region_id: int):
+        self.shipping_id = shipping_id
+        self.shipping_type = shipping_type
+        self.shipping_cost = shipping_cost
+        self.shipping_region_id = shipping_region_id
+
+    def update_shipping_info(self):
+        pass
